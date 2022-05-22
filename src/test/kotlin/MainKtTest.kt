84ab850a59e7fc32a97bc21a27109502119b8335
Naufal Aliraqi <aliraqixnaufal1998@gmail.com>
2022-05-22 13:02:11 +0300

    edit text of given comment

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,7 +6,7 @@ internal class MainKtTest {
 
     @Test
     fun should_ReturnCorrectPercentage_When_AllStringsInArrayCorrect() {
-        // given an array of only a,b string
+        // given an array of only a,b strings
         val arrayOfStringToCheck: Array<String?> = arrayOf("a", "b", "b")
         // when check array of strings
         val resultOfCheck = checkString(arrayOfStringToCheck)
@@ -16,7 +16,7 @@ internal class MainKtTest {
 
     @Test
     fun should_ReturnIncorrect_When_ArrayHaveStringOf_a_LongerThanOneCharacter() {
-        // given an array of only a,b string
+        // given an array have string of a longer than one character
         val arrayOfStringToCheck: Array<String?> = arrayOf("aa", "b", "b")
         // when check array of strings
         val resultOfCheck = checkString(arrayOfStringToCheck)
@@ -26,7 +26,7 @@ internal class MainKtTest {
 
     @Test
     fun should_ReturnIncorrect_When_ArrayHaveStringOf_b_LongerThanOneCharacter() {
-        // given an array of only a,b string
+        // given an array have string of b longer than one character
         val arrayOfStringToCheck: Array<String?> = arrayOf("a", "bbb", "b")
         // when check array of strings
         val resultOfCheck = checkString(arrayOfStringToCheck)
@@ -36,7 +36,7 @@ internal class MainKtTest {
 
     @Test
     fun should_ReturnIncorrect_When_ArrayHaveStringOfNullCharacter() {
-        // given an array of only a,b string
+        // given an array have string of null character
         val arrayOfStringToCheck: Array<String?> = arrayOf("a", "b", null)
         // when check array of strings
         val resultOfCheck = checkString(arrayOfStringToCheck)
@@ -46,7 +46,7 @@ internal class MainKtTest {
 
     @Test
     fun should_ReturnIncorrect_When_ArrayHaveStringOfIncorrectCharacter(){
-        // given an array of only a,b string
+        // given an array have string of incorrect character
         val arrayOfStringToCheck: Array<String?> = arrayOf("a", "b", "p")
         // when check array of strings
         val resultOfCheck = checkString(arrayOfStringToCheck)
